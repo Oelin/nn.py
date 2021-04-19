6e9aea68c16503930ae900f1be32bb10360c069f
Oelin <42823429+Oelin@users.noreply.github.com>
2021-04-19 11:04:13 +0100

    Update nn.py

--- nn.py
+++ nn.py
@@ -23,16 +23,16 @@ Y = array([[1, 0, 1, 0, 1, 0, 0, 0]]).T
 
 class nn:
     def __init__(self, *shape):
-        self.edges = array([*map(randn, shape[:-1], shape[1:])], dtype=object)
-
+        self.links = array([*map(randn, shape[:-1], shape[1:])], dtype=object)
+        
 
     def forward(self, x):
-        net = [ x ]
+        outs = [ x ]
 
-        for i, edges in enumerate(self.edges):
-            net.append(A(net[i] @ edges))
+        for i, links in enumerate(self.links):
+            outs.append(A(net[i] @ links))
 
-        return net
+        return outs
 
 
     def back(self, net, y):
@@ -41,7 +41,7 @@ class nn:
 
         for i, layer in reversed(list(enumerate(net))[1:-1]):
 
-            loss = layer * (1 - layer) * (loss @ self.edges[i].T)
+            loss = layer * (1 - layer) * (loss @ self.links[i].T)
             prev = net[i - 1]
 
             yield average(prev[:, :, newaxis] * loss[:, newaxis, :], axis=0)
@@ -50,7 +50,7 @@ class nn:
     def fit(self, x, y, time):
 
         for i in range(time):
-          self.edges += -0.5 * array(list(reversed(list(self.back(self.forward(x), y)))), dtype=object)
+          self.links += -0.5 * array(list(reversed(list(self.back(self.forward(x), y)))), dtype=object)
 
 
     def predict(self, x):
