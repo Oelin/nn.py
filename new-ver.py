55cbb78f1dda16e721e3558c961ce5a7f3f1bf72
Oelin <42823429+Oelin@users.noreply.github.com>
2021-04-20 11:36:06 +0100

    Create new-ver.py

---
+++ new-ver.py
@@ -0,0 +1,34 @@
+from numpy import array, average
+from numpy.random import seed, rand
+
+
+A = lambda x: x * (x > 0)
+m = lambda x: 1 * (x > 0)
+
+
+class nn:
+
+    def __init__(self, shape):
+        self.links = array([*map(rand, shape[:-1], shape[1:])])
+
+
+    def forward(self, x):
+
+        for i, links in enumerate(self.links):
+            x.append(A(x[i] @ links))
+
+
+    def back(self, x, y):
+      pass
+
+
+    def fit(self, x, y, rate=.1):
+
+        self.forward(x)
+        self.back(x, y)
+        self.links -= rate * self.change
+
+
+    def predict(self, x):
+        self.forward(x)
+        return x[-1]
